780e3459551ed8dafa9420f2e14847e6b3bb1d12
aly elraziny <alyelraziny@gmail.com>
2023-04-15 16:46:58 +0200

    first commit

---
+++ src/main/java/com/try2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.try2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    //create mapping for controller
+    @GetMapping("/hello")
+    public String sayHello(Model theModel ){
+
+        theModel.addAttribute("theDate" , new java.util.Date());
+
+        return "helloworld" ;
+
+    }
+
+}
